6a640730c03fcb4a4fc01875a28e0cea4ec84617
Isaac Sheridan <138263266+NewDevCoding@users.noreply.github.com>
2024-02-29 20:06:39 -0500

    put timer back to remove error

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -103,6 +103,11 @@ var config_data = `
       "defaultValue": "x"
     }
   ],
+  "endgame": [
+    { "name": "Stage Timer",
+      "code": "dt",
+      "type": "timer"
+    },
     { "name": "Final Status",
       "code": "fs",
       "type":"radio",
